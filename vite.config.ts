387a22eb03ef0e2f6b5da6551fae6977f0bed5a0
Probir Sarkar <probirsarkar855@gmail.com>
2026-06-17 21:36:53 +0530

    full bun

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,9 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    nitro(),
+    nitro({
+      preset: 'bun',
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
