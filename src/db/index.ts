db63ef6d746d41e3cf29ccbd1dbf471611bd1d0c
Probir Sarkar <probirsarkar855@gmail.com>
2026-06-26 22:46:54 +0530

    Refactor package.json scripts to remove Bun and use Vite directly; add SSL certificate handling in database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,24 @@
-import { relations } from './relations'
-import * as schema from './schema'
-import { drizzle } from 'drizzle-orm/node-postgres'
-import { env } from '#/env'
+import { relations } from './relations';
+import * as schema from './schema';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { env } from '#/env';
+import { sql } from 'drizzle-orm';
+import { Pool } from 'pg';
+import { readFileSync } from 'node:fs';
 
-export const db = drizzle(env.DATABASE_URL, {
+const ca = readFileSync(new URL('./ca.pem', import.meta.url), 'utf8')
+
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: ca,
+  },
+})
+
+export const db = drizzle({
   schema,
   relations,
+  client: pool,
 })
