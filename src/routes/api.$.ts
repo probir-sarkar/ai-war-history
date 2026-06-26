570333ffe7806032c1ab5b1e6c129df50a15bfd8
Probir Sarkar <probirsarkar855@gmail.com>
2026-06-26 23:33:03 +0530

    Remove unused dependencies and OpenAPIReferencePlugin from API route handler

--- src/routes/api.$.ts
+++ src/routes/api.$.ts
@@ -5,9 +5,7 @@ import { ZodToJsonSchemaConverter } from '@orpc/zod/zod4'
 import { SmartCoercionPlugin } from '@orpc/json-schema'
 import { createFileRoute } from '@tanstack/react-router'
 import { onError } from '@orpc/server'
-import { OpenAPIReferencePlugin } from '@orpc/openapi/plugins'
 
-import { TodoSchema } from '#/orpc/schema'
 import router from '#/orpc/router'
 
 const handler = new OpenAPIHandler(router, {
@@ -19,37 +17,6 @@ const handler = new OpenAPIHandler(router, {
   plugins: [
     new SmartCoercionPlugin({
       schemaConverters: [new ZodToJsonSchemaConverter()],
-    }),
-    new OpenAPIReferencePlugin({
-      schemaConverters: [new ZodToJsonSchemaConverter()],
-      specGenerateOptions: {
-        info: {
-          title: 'TanStack ORPC Playground',
-          version: '1.0.0',
-        },
-        commonSchemas: {
-          Todo: { schema: TodoSchema },
-          UndefinedError: { error: 'UndefinedError' },
-        },
-        security: [{ bearerAuth: [] }],
-        components: {
-          securitySchemes: {
-            bearerAuth: {
-              type: 'http',
-              scheme: 'bearer',
-            },
-          },
-        },
-      },
-      docsConfig: {
-        authentication: {
-          securitySchemes: {
-            bearerAuth: {
-              token: 'default-token',
-            },
-          },
-        },
-      },
     }),
   ],
 })
